3348240cfa71c0b7e4cf0ce7562ff0769390b13f
shin-carpediem <buru.aoshin@gmail.com>
2023-07-13 09:33:12 +0900

    ios14で使えるタイプを追加

--- TypoSan/TypoSan/ContentView.swift
+++ TypoSan/TypoSan/ContentView.swift
@@ -4,13 +4,16 @@ struct ContentView: View {
     @State private var sizeType: Int = 1
     
     @State private var largeTitleSize: CGFloat = 0
-    @State private var titleSize: CGFloat = 0
+    @State private var title1Size: CGFloat = 0
+    @State private var title2Size: CGFloat = 0
+    @State private var title3Size: CGFloat = 0
     @State private var headLineSize: CGFloat = 0
     @State private var bodySize: CGFloat = 0
     @State private var calloutSize: CGFloat = 0
     @State private var subheadSize: CGFloat = 0
     @State private var footnoteSize: CGFloat = 0
-    @State private var captionSize: CGFloat = 0
+    @State private var caption1Size: CGFloat = 0
+    @State private var caption2Size: CGFloat = 0
     
     // MARK: - View
     
@@ -45,22 +48,34 @@ struct ContentView: View {
    
     private var staticTypeSection: some View {
         Section {
-            Text("\(largeTitleSize.int)pt: Large Title Large Title Large Title")
-                .font(.system(size: largeTitleSize))
-            Text("\(titleSize.int)pt: Title 1 Title 1 Title 1 Title 1 Title 1")
-                .font(.system(size: titleSize))
-            Text("\(headLineSize.int)pt: Headline Headline Headline Headline Headline")
-                .font(.system(size: headLineSize, weight: .semibold))
-            Text("\(bodySize.int)pt: Body Body Body Body Body Body Body Body")
-                .font(.system(size: bodySize))
-            Text("\(calloutSize.int)pt: Callout Callout Callout Callout Callout Callout")
-                .font(.system(size: calloutSize))
-            Text("\(subheadSize.int)pt: Subhead Subhead Subhead Subhead Subhead")
-                .font(.system(size: subheadSize))
-            Text("\(footnoteSize.int)pt: Footnone Footnone Footnone Footnone Footnone Footnone")
-                .font(.system(size: footnoteSize))
-            Text("\(captionSize.int)pt: Caption 1 Caption 1 Caption 1 Caption 1 Caption 1 Caption 1")
-                .font(.system(size: captionSize))
+            Group {
+                Text("\(largeTitleSize.int)pt: Large Title Large Title Large Title")
+                    .font(.system(size: largeTitleSize))
+                Text("\(title1Size.int)pt: Title 1 Title 1 Title 1 Title 1 Title 1")
+                    .font(.system(size: title1Size))
+                Text("\(title2Size.int)pt: Title 2 Title 2 Title 2 Title 2 Title 2")
+                    .font(.system(size: title2Size))
+                Text("\(title3Size.int)pt: Title 3 Title 3 Title 3 Title 3 Title 3")
+                    .font(.system(size: title3Size))
+            }
+            Group {
+                Text("\(headLineSize.int)pt: Headline Headline Headline Headline Headline")
+                    .font(.system(size: headLineSize, weight: .semibold))
+                Text("\(bodySize.int)pt: Body Body Body Body Body Body Body Body")
+                    .font(.system(size: bodySize))
+                Text("\(calloutSize.int)pt: Callout Callout Callout Callout Callout Callout")
+                    .font(.system(size: calloutSize))
+                Text("\(subheadSize.int)pt: Subhead Subhead Subhead Subhead Subhead")
+                    .font(.system(size: subheadSize))
+                Text("\(footnoteSize.int)pt: Footnone Footnone Footnone Footnone Footnone Footnone")
+                    .font(.system(size: footnoteSize))
+            }
+            Group {
+                Text("\(caption1Size.int)pt: Caption 1 Caption 1 Caption 1 Caption 1 Caption 1 Caption 1")
+                    .font(.system(size: caption1Size))
+                Text("\(caption2Size.int)pt: Caption 2 Caption 2 Caption 2 Caption 2 Caption 2 Caption 2")
+                    .font(.system(size: caption2Size))
+            }
         } header: {
             Text("STATIC TYPE")
         }
@@ -68,22 +83,34 @@ struct ContentView: View {
     
     private var dynamicTypeSection: some View {
         Section {
-            Text("32pt: Large Title Large Title Large Title")
-                .font(.largeTitle)
-            Text("26pt: Title 1 Title 1 Title 1 Title 1 Title 1")
-                .font(.title)
-            Text("15pt: Headline Headline Headline Headline Headline")
-                .font(.headline)
-            Text("15pt: Body Body Body Body Body Body Body Body")
-                .font(.body)
-            Text("14pt: Callout Callout Callout Callout Callout Callout")
-                .font(.callout)
-            Text("13pt: Subhead Subhead Subhead Subhead Subhead")
-                .font(.subheadline)
-            Text("12pt: Footnone Footnone Footnone Footnone Footnone Footnone")
-                .font(.footnote)
-            Text("11pt: Caption 1 Caption 1 Caption 1 Caption 1 Caption 1 Caption 1")
-                .font(.caption)
+            Group {
+                Text("Large Title Large Title Large Title")
+                    .font(.largeTitle)
+                Text("Title 1 Title 1 Title 1 Title 1 Title 1")
+                    .font(.title)
+                Text("Title 2 Title 2 Title 2 Title 2 Title 2")
+                    .font(.title2)
+                Text("Title 3 Title 3 Title 3 Title 3 Title 3")
+                    .font(.title3)
+            }
+            Group {
+                Text("Headline Headline Headline Headline Headline")
+                    .font(.headline)
+                Text("Body Body Body Body Body Body Body Body")
+                    .font(.body)
+                Text("Callout Callout Callout Callout Callout Callout")
+                    .font(.callout)
+                Text("Subhead Subhead Subhead Subhead Subhead")
+                    .font(.subheadline)
+                Text("Footnone Footnone Footnone Footnone Footnone Footnone")
+                    .font(.footnote)
+            }
+            Group {
+                Text("Caption 1 Caption 1 Caption 1 Caption 1 Caption 1 Caption 1")
+                    .font(.caption)
+                Text("Caption 2 Caption 2 Caption 2 Caption 2 Caption 2 Caption 2")
+                    .font(.caption2)
+            }
         } header: {
             Text("DYNAMIC TYPE")
         }
@@ -94,67 +121,88 @@ extension ContentView {
     private func updateFontSize(sizeType: Int) {
         if sizeType == 0 {
             largeTitleSize = HIGTypeSize.xSmall.sizeSet.largeTitle
-            titleSize = HIGTypeSize.xSmall.sizeSet.title1
+            title1Size = HIGTypeSize.xSmall.sizeSet.title1
+            title2Size = HIGTypeSize.xSmall.sizeSet.title2
+            title3Size = HIGTypeSize.xSmall.sizeSet.title3
             headLineSize = HIGTypeSize.xSmall.sizeSet.headline
             bodySize = HIGTypeSize.xSmall.sizeSet.body
             calloutSize = HIGTypeSize.xSmall.sizeSet.callout
             subheadSize = HIGTypeSize.xSmall.sizeSet.subhead
             footnoteSize = HIGTypeSize.xSmall.sizeSet.footnote
-            captionSize = HIGTypeSize.xSmall.sizeSet.caption1
+            caption1Size = HIGTypeSize.xSmall.sizeSet.caption1
+            caption2Size = HIGTypeSize.xSmall.sizeSet.caption2
         } else if sizeType == 1 {
             largeTitleSize = HIGTypeSize.small.sizeSet.largeTitle
-            titleSize = HIGTypeSize.small.sizeSet.title1
+            title1Size = HIGTypeSize.small.sizeSet.title1
+            title2Size = HIGTypeSize.small.sizeSet.title2
+            title3Size = HIGTypeSize.small.sizeSet.title3
             headLineSize = HIGTypeSize.small.sizeSet.headline
             bodySize = HIGTypeSize.small.sizeSet.body
             calloutSize = HIGTypeSize.small.sizeSet.callout
             subheadSize = HIGTypeSize.small.sizeSet.subhead
             footnoteSize = HIGTypeSize.small.sizeSet.footnote
-            captionSize = HIGTypeSize.small.sizeSet.caption1
+            caption1Size = HIGTypeSize.small.sizeSet.caption1
+            caption2Size = HIGTypeSize.small.sizeSet.caption2
         } else if sizeType == 2 {
             largeTitleSize = HIGTypeSize.medium.sizeSet.largeTitle
-            titleSize = HIGTypeSize.medium.sizeSet.title1
+            title1Size = HIGTypeSize.medium.sizeSet.title1
+            title2Size = HIGTypeSize.medium.sizeSet.title2
+            title3Size = HIGTypeSize.medium.sizeSet.title3
             headLineSize = HIGTypeSize.medium.sizeSet.headline
             bodySize = HIGTypeSize.medium.sizeSet.body
             calloutSize = HIGTypeSize.medium.sizeSet.callout
             subheadSize = HIGTypeSize.medium.sizeSet.subhead
             footnoteSize = HIGTypeSize.medium.sizeSet.footnote
-            captionSize = HIGTypeSize.medium.sizeSet.caption1
+            caption1Size = HIGTypeSize.medium.sizeSet.caption1
+            caption2Size = HIGTypeSize.medium.sizeSet.caption2
         } else if sizeType == 3 {
             largeTitleSize = HIGTypeSize.largeDefault.sizeSet.largeTitle
-            titleSize = HIGTypeSize.largeDefault.sizeSet.title1
+            title1Size = HIGTypeSize.largeDefault.sizeSet.title1
+            title2Size = HIGTypeSize.largeDefault.sizeSet.title2
+            title3Size = HIGTypeSize.largeDefault.sizeSet.title3
             headLineSize = HIGTypeSize.largeDefault.sizeSet.headline
             bodySize = HIGTypeSize.largeDefault.sizeSet.body
             calloutSize = HIGTypeSize.largeDefault.sizeSet.callout
             subheadSize = HIGTypeSize.largeDefault.sizeSet.subhead
             footnoteSize = HIGTypeSize.largeDefault.sizeSet.footnote
-            captionSize = HIGTypeSize.largeDefault.sizeSet.caption1
+            caption1Size = HIGTypeSize.largeDefault.sizeSet.caption1
+            caption2Size = HIGTypeSize.largeDefault.sizeSet.caption2
         } else if sizeType == 4 {
             largeTitleSize = HIGTypeSize.xLarge.sizeSet.largeTitle
-            titleSize = HIGTypeSize.xLarge.sizeSet.title1
+            title1Size = HIGTypeSize.xLarge.sizeSet.title1
+            title2Size = HIGTypeSize.xLarge.sizeSet.title2
+            title3Size = HIGTypeSize.xLarge.sizeSet.title3
             headLineSize = HIGTypeSize.xLarge.sizeSet.headline
             bodySize = HIGTypeSize.xLarge.sizeSet.body
             calloutSize = HIGTypeSize.xLarge.sizeSet.callout
             subheadSize = HIGTypeSize.xLarge.sizeSet.subhead
             footnoteSize = HIGTypeSize.xLarge.sizeSet.footnote
-            captionSize = HIGTypeSize.xLarge.sizeSet.caption1
+            caption1Size = HIGTypeSize.xLarge.sizeSet.caption1
+            caption2Size = HIGTypeSize.xLarge.sizeSet.caption2
         } else if sizeType == 5 {
             largeTitleSize = HIGTypeSize.xxLarge.sizeSet.largeTitle
-            titleSize = HIGTypeSize.xxLarge.sizeSet.title1
+            title1Size = HIGTypeSize.xxLarge.sizeSet.title1
+            title2Size = HIGTypeSize.xxLarge.sizeSet.title2
+            title3Size = HIGTypeSize.xxLarge.sizeSet.title3
             headLineSize = HIGTypeSize.xxLarge.sizeSet.headline
             bodySize = HIGTypeSize.xxLarge.sizeSet.body
             calloutSize = HIGTypeSize.xxLarge.sizeSet.callout
             subheadSize = HIGTypeSize.xxLarge.sizeSet.subhead
             footnoteSize = HIGTypeSize.xxLarge.sizeSet.footnote
-            captionSize = HIGTypeSize.xxLarge.sizeSet.caption1
+            caption1Size = HIGTypeSize.xxLarge.sizeSet.caption1
+            caption2Size = HIGTypeSize.xxLarge.sizeSet.caption2
         } else if sizeType == 6 {
             largeTitleSize = HIGTypeSize.xxxLarge.sizeSet.largeTitle
-            titleSize = HIGTypeSize.xxxLarge.sizeSet.title1
+            title1Size = HIGTypeSize.xxxLarge.sizeSet.title1
+            title2Size = HIGTypeSize.xxxLarge.sizeSet.title2
+            title3Size = HIGTypeSize.xxxLarge.sizeSet.title3
             headLineSize = HIGTypeSize.xxxLarge.sizeSet.headline
             bodySize = HIGTypeSize.xxxLarge.sizeSet.body
             calloutSize = HIGTypeSize.xxxLarge.sizeSet.callout
             subheadSize = HIGTypeSize.xxxLarge.sizeSet.subhead
             footnoteSize = HIGTypeSize.xxxLarge.sizeSet.footnote
-            captionSize = HIGTypeSize.xxxLarge.sizeSet.caption1
+            caption1Size = HIGTypeSize.xxxLarge.sizeSet.caption1
+            caption2Size = HIGTypeSize.xxxLarge.sizeSet.caption2
         } else {
             return
         }
--- TypoSan/TypoSan/HIGTypeSizeSet.swift
+++ TypoSan/TypoSan/HIGTypeSizeSet.swift
@@ -3,12 +3,15 @@ import Foundation
 struct HIGTypeSizeSet {
     var largeTitle: CGFloat
     var title1: CGFloat
+    var title2: CGFloat
+    var title3: CGFloat
     var headline: CGFloat
     var body: CGFloat
     var callout: CGFloat
     var subhead: CGFloat
     var footnote: CGFloat
     var caption1: CGFloat
+    var caption2: CGFloat
 }
 
 enum HIGTypeSize {
@@ -23,25 +26,25 @@ enum HIGTypeSize {
     var sizeSet: HIGTypeSizeSet {
         switch self {
         case .xSmall:
-            return HIGTypeSizeSet(largeTitle: 31, title1: 25, headline: 14, body: 14, callout: 13, subhead: 12, footnote: 12, caption1: 11)
+            return HIGTypeSizeSet(largeTitle: 31, title1: 25, title2: 19, title3: 17, headline: 14, body: 14, callout: 13, subhead: 12, footnote: 12, caption1: 11, caption2: 11)
             
         case .small:
-            return HIGTypeSizeSet(largeTitle: 32, title1: 26, headline: 15, body: 15, callout: 14, subhead: 13, footnote: 12, caption1: 11)
+            return HIGTypeSizeSet(largeTitle: 32, title1: 26, title2: 20, title3: 18, headline: 15, body: 15, callout: 14, subhead: 13, footnote: 12, caption1: 11, caption2: 11)
             
         case .medium:
-            return HIGTypeSizeSet(largeTitle: 33, title1: 27, headline: 16, body: 15, callout: 15, subhead: 14, footnote: 12, caption1: 11)
+            return HIGTypeSizeSet(largeTitle: 33, title1: 27, title2: 21, title3: 19, headline: 16, body: 15, callout: 15, subhead: 14, footnote: 12, caption1: 11, caption2: 11)
             
         case .largeDefault:
-            return HIGTypeSizeSet(largeTitle: 34, title1: 28, headline: 17, body: 17, callout: 16, subhead: 15, footnote: 13, caption1: 12)
+            return HIGTypeSizeSet(largeTitle: 34, title1: 28, title2: 22, title3: 20, headline: 17, body: 17, callout: 16, subhead: 15, footnote: 13, caption1: 12, caption2: 11)
             
         case .xLarge:
-            return HIGTypeSizeSet(largeTitle: 36, title1: 30, headline: 19, body: 19, callout: 18, subhead: 17, footnote: 15, caption1: 14)
+            return HIGTypeSizeSet(largeTitle: 36, title1: 30, title2: 24, title3: 22, headline: 19, body: 19, callout: 18, subhead: 17, footnote: 15, caption1: 14, caption2: 13)
             
         case .xxLarge:
-            return HIGTypeSizeSet(largeTitle: 38, title1: 32, headline: 21, body: 21, callout: 20, subhead: 19, footnote: 17, caption1: 16)
+            return HIGTypeSizeSet(largeTitle: 38, title1: 32, title2: 26, title3: 24, headline: 21, body: 21, callout: 20, subhead: 19, footnote: 17, caption1: 16, caption2: 15)
             
         case .xxxLarge:
-            return HIGTypeSizeSet(largeTitle: 40, title1: 34, headline: 23, body: 23, callout: 22, subhead: 21, footnote: 19, caption1: 18)
+            return HIGTypeSizeSet(largeTitle: 40, title1: 34, title2: 28, title3: 26, headline: 23, body: 23, callout: 22, subhead: 21, footnote: 19, caption1: 18, caption2: 17)
         }
     }
 }
